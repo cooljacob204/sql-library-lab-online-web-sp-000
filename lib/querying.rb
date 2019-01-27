aeb8076f5319e6f2e19dfeab21e0a30110edc60f
Jacob Feldman <jacobgfeld@gmail.com>
2019-01-27 18:38:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) from characters"
+  "SELECT characters.species, COUNT(characters.species) from characters GROUP BY character.species"
 end
 
 def select_name_and_series_subgenres_of_authors
